9a0824e74490c6c5dd041b7825779c878a432f1c
Kristin Ponsonby <kristinponsonby@gmail.com>
2022-09-19 08:30:59 -0400

    Skill component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
@@ -30,10 +31,12 @@ const Home: NextPage = () => {
       </section>
 
       <section id="skills" className="snap-start">
-        <Skills />
+        <Skills /> 
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
 
